adaf57141f90b936b71f0641321c0727bbbf0a2e
Suraj Jaiswal <surazjaiswal@gmail.com>
2022-01-17 12:03:46 +0530

    Favicon Modes added to Dark & Light mode

--- 5.Light Dark Mode/script.js
+++ 5.Light Dark Mode/script.js
@@ -7,29 +7,70 @@ const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
+const flink1 = document.getElementById("link1");
+const flink2 = document.getElementById("link2");
+const flink3 = document.getElementById("link3");
+const title = document.getElementById("title");
+
+// for favicon dark mode
+function favToDark() {
+  title.innerText = "Dark Mode";
+  flink1.href = "faviconDark/apple-touch-icon.png";
+  flink2.href = "faviconDark/favicon-32x32.png";
+  flink3.href = "faviconDark/favicon-16x16.png";
+}
+// for favicon light mode
+function favToLight() {
+  title.innerText = "Light Mode";
+  flink1.href = "faviconLight/apple-touch-icon.png";
+  flink2.href = "faviconLight/favicon-32x32.png";
+  flink3.href = "faviconLight/favicon-16x16.png";
+}
+
+//  for images to dark mode
+function imgToDark() {
+  image1.src = "img/undraw_feeling_proud_dark.svg";
+  image2.src = "img/undraw_conceptual_idea_dark.svg";
+  image3.src = "img/undraw_proud_coder_dark.svg";
+}
+
+// for images to light mode
+function imgToLight() {
+  image1.src = "img/undraw_feeling_proud_light.svg";
+  image2.src = "img/undraw_conceptual_idea_light.svg";
+  image3.src = "img/undraw_proud_coder_light.svg";
+}
+
+// for toggle icon light mode
+function toggleLight() {
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.remove("fa-moon");
+  toggleIcon.children[1].classList.add("fa-sun");
+}
+
+// for toggle icon dark mode
+function toggleDark() {
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+}
 
 // Dark Mode
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.remove("fa-sun");
-  toggleIcon.children[1].classList.add("fa-moon");
-  image1.src = "img/undraw_feeling_proud_dark.svg";
-  image2.src = "img/undraw_conceptual_idea_dark.svg";
-  image3.src = "img/undraw_proud_coder_dark.svg";
+  toggleDark();
+  imgToDark();
+  favToDark();
 }
 
 // Light Mode
 function lightMode() {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.remove("fa-moon");
-  toggleIcon.children[1].classList.add("fa-sun");
-  image1.src = "img/undraw_feeling_proud_light.svg";
-  image2.src = "img/undraw_conceptual_idea_light.svg";
-  image3.src = "img/undraw_proud_coder_light.svg";
+  toggleLight();
+  imgToLight();
+  favToLight();
 }
 
 function switchTheme(event) {
